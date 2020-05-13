062639dde7142b20463b47d2eba47bcdb7198559
TERRENCE LAI <terrencelai@TERRENCEs-MacBook-Air.local>
2020-05-12 21:36:50 -0400

    add render on scroll

--- src/index.js
+++ src/index.js
@@ -6,8 +6,36 @@ import { categoryGrowth } from './scripts/render_chart_cat_growth';
 
 window.addEventListener("DOMContentLoaded", () => {
 
-  sectorTotals();
-  categoryRankings();
-  categoryGrowth();
+  const isInViewport = (ele) => {
+    const bounding = ele.getBoundingClientRect();
+    return(
+      bounding.top + 150 <= (window.innerHeight || document.documentElement.clientHeight)
+    )
+  }
+
+  const totals = document.querySelector('#sector-totals');
+  const rankings = document.querySelector('#category-totals');
+  const growth = document.querySelector('#category-growth');
+
+  let totalsLoaded = false;
+  let rankingsLoaded = false;
+  let growthLoaded = false;
+
+  window.addEventListener('scroll', function (event) {
+    if (!totalsLoaded && isInViewport(totals)) {
+      sectorTotals();
+      totalsLoaded = true;
+    }
+
+    if (!rankingsLoaded && isInViewport(rankings))  {
+      categoryRankings();
+      rankingsLoaded = true;
+    }
+
+    if (!growthLoaded && isInViewport(growth)) {
+      categoryGrowth();
+      growthLoaded = true;
+    }
+  })
     
 });
